00e16e79740b49713971dc9232f799212316c536
Brennanlehman <150840778+Brennanlehman@users.noreply.github.com>
2024-03-26 10:02:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,94 @@
+# --------------------------------------------
+# Imports at the top - PyShiny EXPRESS VERSION
+# --------------------------------------------
 
-from collections import deque
+from shiny import reactive, render
+from shiny.express import ui
+import random
+from datetime import datetime
+from faicons import icon_svg
 
-# Create an empty deque
-empty_deque = deque()
-print(empty_deque) 
+# --------------------------------------------
+# SET UP THE REACIVE CONTENT
+# --------------------------------------------
 
-# Create a deque by passing in a list with values
-temp_deque_F = deque( [56, 58, 47, 54, 55] )
-print(temp_deque_F)  
+# --------------------------------------------
+# First, set a constant UPDATE INTERVAL for all live data
+# Constants are usually defined in uppercase letters
+# Use a type hint to make it clear that it's an integer (: int)
+# --------------------------------------------
+UPDATE_INTERVAL_SECS: int = 1
+# --------------------------------------------
 
-# Create a deque by passing in a list variable
-temp_list_C = [5, 6, 8, 4, 3, 2]
-temp_deque_C = deque( temp_list_C )
-print(temp_deque_C )
+# Initialize a REACTIVE CALC that our display components can call
+# to get the latest data and display it.
+# The calculation is invalidated every UPDATE_INTERVAL_SECS
+# to trigger updates.
 
-temp_deque_F.append(60)
-temp_deque_F.append(62)
-temp_deque_F.append(64)
-temp_deque_F.append(61)
-print(temp_deque_F)
+@reactive.calc()
+def reactive_calc_combined():
 
-temp_deque_F.pop()  
-temp_deque_F.popleft()
-print(temp_deque_F)
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
 
-len(temp_deque_F) 
-print(len(temp_deque_F))
+    # Data generation logic. Get random between -18 and -16 C, rounded to 1 decimal place
+    temp = round(random.uniform(-18, -10), 1)
+
+    # Get a timestamp for "now" and use string format strftime() method to format it
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
+
+    # Return everything we need
+    return latest_dictionary_entry
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Page Options
+# ------------------------------------------------
+
+# Set title to a string in quotes that will appear at the top
+# Set fillable to True to use the whole page width for the UI
+
+ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Sidebar
+# ------------------------------------------------
+
+# Sidebar is typically used for user interaction/information
+# Note the with statement to create the sidebar followed by a colon
+# Everything in the sidebar is indented consistently
+
+with ui.sidebar(open="open"):
+    ui.h2("Antarctic Explorer", class_="text-center")
+    ui.p(
+        "A demonstration of real-time temperature readings in Antarctica.",
+        class_="text-center",
+    )
+
+#---------------------------------------------------------------------
+# In Shiny Express, everything not in the sidebar is in the main panel
+#---------------------------------------------------------------------
+
+
+ui.h2("Current Temperature")
+
+@render.text
+def display_temp():
+    """Get the latest reading and return a temperature string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['temp']} C"
+
+ui.p("chance of severe weather")
+
+icon_svg("bolt")
+
+ui.hr()
+
+ui.h2("Current Date and Time")
+
+@render.text
+def display_time():
+    """Get the latest reading and return a timestamp string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['timestamp']}"
